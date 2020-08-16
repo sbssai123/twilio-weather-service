e20457ade4598bb162d6616413e5ce3520c4c5b1
sbssai123 <sbs.sai@gmail.com>
2020-08-16 11:26:45 -0400

    convert response -> struct

--- weather.go
+++ weather.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 )
@@ -10,21 +11,41 @@ const (
 	BASE_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 )
 
-func getTodaysForecast(cityName string) {
+type WeatherData struct {
+	Temperature     TemperatureData `json:"main"`
+	CityName        string          `json:"name"`
+	FullDescription []struct {
+		Description string `json:"description"`
+	} `json:"weather"`
+}
+
+type TemperatureData struct {
+	CurrentTemperature   float64 `json:"temp,omitempty"`
+	FeelsLikeTemperature float64 `json:"feels_like,omitempty"`
+	MinTemperature       float64 `json:"temp_min,omitempty"`
+	MaxTemperature       float64 `json:"temp_max,omitempty"`
+	Humidity             int     `json:"humidity,omitempty"`
+}
+
+func getTodaysForecast(cityName string) (WeatherData, error) {
 
 	req, err := http.NewRequest("GET", BASE_API_URL, nil)
 	q := req.URL.Query()
 	q.Add("q", cityName)
-	q.Add("appid", "<API_KEY>")
+	q.Add("appid", "973a83b25899377e24c6d563fcb84ad2")
 	q.Add("units", "imperial")
 	req.URL.RawQuery = q.Encode()
 	req.Close = true
 
 	resp, err := http.DefaultClient.Do(req)
+	var weatherData WeatherData
 	if err != nil {
-		fmt.Printf("Request failed with error %s \n", err)
+		return weatherData, errors.New("Unable to execute request to Weather API")
 	} else {
-		data, _ := ioutil.ReadAll(resp.Body)
-		fmt.Println(string(data))
+		body, _ := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+
+		json.Unmarshal(body, &weatherData)
+		return weatherData, nil
 	}
 }
